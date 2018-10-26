76b1c1950684b66db8d99167d9a6db6cbd80e70f
itweleve <44469840+itweleve@users.noreply.github.com>
2018-10-26 13:04:30 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBAw0OqHeVsormACMG8nycaXal1sIF8KUA');
 define('GJ_CODE', 'US');
-define('SITE_NAME', 'SSame的私人站点');
-define('TITLENAME', '国华测试');
+define('SITE_NAME', '私人站点');
+define('TITLENAME', '学好知识，为建设新中国做准备');
 define('EN2DEKEY', 'lujianbupingyishenghou');
 define('EMAIL', 'wshydzzzuoshang@gmail.com');
 ?>
